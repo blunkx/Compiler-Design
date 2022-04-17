ff07764c9b7bf700b8e32629b190c1997e453046
blunkx <asd9521889@gmail.com>
2022-04-17 14:27:00 +0800

    add symbol table

---
+++ lex/symbols.c
@@ -0,0 +1,90 @@
+#include "symbols.h"
+#define copy_str(des, src)                        \
+    {                                             \
+        des = malloc(sizeof(char) * strlen(src)); \
+        des = strcpy(des, src);                   \
+    }
+symbol_table create()
+{
+    symbol_table temp;
+    temp.begin = NULL;
+    temp.size = 0;
+    return temp;
+}
+int lookup(char *sym, symbol_table tb)
+{
+    int index = 0;
+    symbol *temp_ptr = tb.begin;
+    while (temp_ptr != NULL)
+    {
+        if (strcmp(sym, temp_ptr->name) != 0)
+        {
+            index += 1;
+        }
+        else
+            return index;
+        temp_ptr = temp_ptr->nptr;
+    }
+    return -1;
+}
+int insert(char *_n, char *_type, char *_scope, symbol_table *tb)
+{
+    symbol *temp_ptr = tb->begin;
+    if (temp_ptr == NULL)
+    {
+        // symbol temp = {.name = _n, .type = _type, .scope = _scope, .pptr =
+        // NULL, .nptr = NULL}; tb->begin = &temp;
+        symbol *temp = malloc(sizeof(symbol));
+        copy_str(temp->name, _n);
+        copy_str(temp->type, _type);
+        copy_str(temp->scope, _scope);
+        temp->pptr = NULL;
+        temp->nptr = NULL;
+        // strcpy(temp->name, _n);
+        tb->begin = temp;
+        tb->size += 1;
+    }
+    else
+    {
+        while (temp_ptr->nptr != NULL)
+        {
+            temp_ptr = temp_ptr->nptr;
+        }
+        symbol *temp = malloc(sizeof(symbol));
+        copy_str(temp->name, _n);
+        copy_str(temp->type, _type);
+        copy_str(temp->scope, _scope);
+        temp->pptr = temp_ptr;
+        temp->nptr = NULL;
+        // strcpy(temp->name, _n);
+        temp_ptr->nptr = temp;
+        tb->size += 1;
+    }
+    return 0;
+}
+
+void dump(symbol_table *tb)
+{
+    symbol *temp_ptr = tb->begin;
+    tb->begin = NULL;
+    tb->size = 0;
+    symbol *cur_ptr;
+    while (temp_ptr != NULL)
+    {
+        cur_ptr = temp_ptr;
+        temp_ptr = temp_ptr->nptr;
+        free(cur_ptr);
+    }
+}
+void print_tb(symbol_table tb)
+{
+    printf("\nSymbol Table:\n");
+    symbol *temp_ptr = tb.begin;
+    printf("%-12s|%-10s|%-10s\n", "Name:", "Type:", "Scope:");
+    while (temp_ptr != NULL)
+    {
+        printf("%-12s|%-10s|%-10s\n", temp_ptr->name, temp_ptr->type, temp_ptr->scope);
+        // printf("%p\n", temp_ptr);
+        temp_ptr = temp_ptr->nptr;
+    }
+}
